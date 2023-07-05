93524b2a3e48b9e95b6177a80a6a02fa428f5642
Azamat Tavitov <aztapps@gmail.com>
2023-07-05 20:22:56 +0300

    add 3d test plot

--- co2/read_serial.py
+++ co2/read_serial.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib import style
 from datetime import datetime
+from mpl_toolkits import mplot3d
 
 import matplotlib.dates as mdates
 
@@ -15,7 +16,7 @@ PORT = 'COM5'
 plant_number = 0
 people_number = 1
 
-ser = serial.Serial(PORT, 9600, timeout=1)
+#ser = serial.Serial(PORT, 9600, timeout=1)
 time.sleep(2)
 
 df = pd.DataFrame([], columns=["CO2", "TVOC", "Moisture", "datetime"])
@@ -63,16 +64,18 @@ def read_csv(filename):
     df = pd.read_csv(filename)
     #print(datetime.strptime(df["datetime"].loc[0], '%Y-%m-%d %H:%M:%S.%f'))
     
-    plot_static()
+    #plot_static()
+    plot_static3D()
 
 
 def plot_static():
     x = df["datetime"].values.tolist()
     y = df["CO2"].values.tolist()
     z = df["TVOC"].values.tolist()
-    u = df["Moisture"].values.to_list()
+    u = df["Moisture"].values.tolist()
     plt.plot(x, y, label="CO2", color="red")
-    plt.plot(x, z, label="TVOC", color="blue")
+    plt.plot(x, z, label="TVOC", color="black")
+    plt.plot(x, u, label="Moist", color="blue")
     plt.xlabel('Datetime', fontsize=8)
 
     plt.ylabel('CO2, ppm')
@@ -83,6 +86,35 @@ def plot_static():
 
     #plt.autoscale()
     plt.savefig(f"plot-{int(time.time())}.png")
+    plt.show()
+
+def f(x, y):
+    return np.sin(np.sqrt(x ** 2 + y ** 2))
+
+def plot_static3D():
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+
+    # Data for a three-dimensional line
+    zline = np.linspace(0, 15, 1000)
+    xline = np.sin(zline)
+    yline = np.cos(zline)
+    ax.plot3D(xline, yline, zline, 'gray')
+
+    # Data for three-dimensional scattered points
+    zdata = 15 * np.random.random(100)
+    xdata = np.sin(zdata) + 0.1 * np.random.randn(100)
+    ydata = np.cos(zdata) + 0.1 * np.random.randn(100)
+    ax.scatter3D(xdata, ydata, zdata, c=zdata, cmap='Greens')
+    # surface
+    x = np.linspace(-6, 6, 30)
+    y = np.linspace(-6, 6, 30)
+    X, Y = np.meshgrid(x, y)
+    Z = f(X, Y)
+    print([])
+
+    ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap='viridis', edgecolor='none')
+
     plt.show()
 
 
@@ -108,8 +140,8 @@ def plot_realtime(i, xs, ys):
 
 
 if __name__ == "__main__":
-    read_serial_data()
-    # read_csv("data.csv")
+    #read_serial_data()
+    read_csv("data.csv")
 
     #ani = animation.FuncAnimation(fig, plot_realtime, fargs=(xs, ys), interval=1)
     #plt.show()
