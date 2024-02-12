eaf1f5d80bb09c7b5aae999e7e49c5f32e29c981
Azamat Tavitov <85460283+m112521@users.noreply.github.com>
2024-02-12 17:52:29 +0300

    add pure imu example

---
+++ iot/genuino_IMU.ino
@@ -0,0 +1,34 @@
+#include "CurieIMU.h"
+#include <MadgwickAHRS.h>
+
+Madgwick filter;
+int factor = 800;
+
+int ax, ay, az, gx, gy, gz;
+volatile float yaw, pitch, roll;
+
+
+void setup() {
+  CurieIMU.begin();
+  CurieIMU.setAccelerometerRange(2);
+
+  CurieIMU.autoCalibrateGyroOffset();
+  Serial.println(" Done");  
+  Serial.print("Starting Acceleration calibration...");
+  CurieIMU.autoCalibrateAccelerometerOffset(X_AXIS, 0);
+  CurieIMU.autoCalibrateAccelerometerOffset(Y_AXIS, 0);
+  CurieIMU.autoCalibrateAccelerometerOffset(Z_AXIS, 1);
+}
+
+void loop() {
+  CurieIMU.readMotionSensor(ax, ay, az, gx, gy, gz); 
+  filter.updateIMU(gx / factor, gy / factor, gz / factor, ax, ay, az);
+  yaw = filter.getYaw();
+  pitch = filter.getPitch();
+  roll = filter.getRoll();
+
+  if (roll > 100) {
+    tone(8, 1024, 100);
+    delay(100);
+  }
+}
